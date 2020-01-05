2b91f2fd8ff7ee053769a88ca674e298594afcc2
kael <cdcp-1795@hotmail.com>
2020-01-05 22:03:49 +0000

    pint

--- op_functions.c
+++ op_functions.c
@@ -73,8 +73,12 @@ void pall_k(stack_t **stack, unsigned int line_number)
  */
 void pint_k(stack_t **stack, unsigned int line_number)
 {
-	(void) stack;
-	(void) line_number;
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
 }
 
 /**
