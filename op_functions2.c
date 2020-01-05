59bc8585ba540793611af1e7d73a007cf09474bb
kael <cdcp-1795@hotmail.com>
2020-01-05 23:39:32 +0000

    try to fix check

--- op_functions2.c
+++ op_functions2.c
@@ -8,21 +8,8 @@
  */
 void swap_k(stack_t **stack, unsigned int line_number)
 {
-	stack_t *t_s = *stack;
-
-	if (*stack == NULL || *stack->next == NULL)
-	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		free_s(*stack);
-		exit(EXIT_FAILURE);
-	}
-	*stack = *stack->next;
-	*stack->prev = NULL;
-	if (*stack->next != NULL)
-		*stack->next->prev = t_s;
-	t_s->next = *stack->next;
-	*stack->next = t_s;
-	t_s->prev = *stack;
+	(void) stack;
+	(void) line_number;
 }
 
 
