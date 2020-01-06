523cea019dabce5a8566b0c2db1dd28c91667a10
kael <cdcp-1795@hotmail.com>
2020-01-06 03:05:54 +0000

    div and mul

--- op_functions3.c
+++ op_functions3.c
@@ -8,8 +8,20 @@
  */
 void div_k(stack_t **stack, unsigned int line_number)
 {
-	(void) stack;
-	(void) line_number;
+	stack_t *t_s = *stack;
+
+	if (!*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (t_s->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	t_s->next->n = t_s->next->n / t_s->n;
+	pop_k(&(*stack), 1);
 }
 
 /**
@@ -40,7 +52,19 @@ void mul_k(stack_t **stack, unsigned int line_number)
  */
 void mod_k(stack_t **stack, unsigned int line_number)
 {
-	(void) stack;
-	(void) line_number;
+	stack_t *t_s = *stack;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (t_s->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	t_s->next->n = t_s->next->n % t_s->n;
+	pop_k(&(*stack), 1);
 }
 
